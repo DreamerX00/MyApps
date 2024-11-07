8b77c016df21d61ddc8caa58b0c96130df505483
DreamerX <akashsinghaa008@gmail.com>
2024-11-07 11:09:02 +0530

    commit 3

--- First_Application/app/build.gradle.kts
+++ First_Application/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.first_application"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.first_application"
